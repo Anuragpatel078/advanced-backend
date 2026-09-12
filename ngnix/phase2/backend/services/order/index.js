49841a7287dcb96902e877c0a3b8f668b0dbad5b
Anuragpatel078 <ap4254893@gmail.com>
2026-09-12 09:43:53 +0530

    ngnix done

---
+++ ngnix/phase2/backend/services/order/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port=process.env.PORT||5000;
+const app=express();
+
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+  return res.status(200).json({
+    message: `hello from order sevice`
+});
+});
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
